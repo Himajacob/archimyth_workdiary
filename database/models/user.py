991804c37f37f69f351cc6861304ed250654a7fd
Himajacob <himajacob221@gmail.com>
2026-04-27 10:34:52 +0530

    user table created with alembic

--- database/models/user.py
+++ database/models/user.py
@@ -1,18 +1,34 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
-from sqlalchemy.sql import func
+from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP, ForeignKey, text, CheckConstraint
 from database.base import Base
 
 
 class User(Base):
     __tablename__ = "users"
+
     id = Column(Integer, primary_key=True, index=True)
     first_name = Column(String(100), nullable=True)
     last_name = Column(String(100), nullable=True)
     email = Column(String(255), unique=True, nullable=False)
     password_hash = Column(String, nullable=True)
     role = Column(String(50), nullable=False)
-    is_active = Column(Boolean, default=True)
+    is_active = Column(Boolean, nullable=False, server_default=text("true"))
     created_by = Column(Integer, ForeignKey("users.id"), nullable=True)
     updated_by = Column(Integer, ForeignKey("users.id"), nullable=True)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    created_at = Column(
+        TIMESTAMP,
+        server_default=text("CURRENT_TIMESTAMP"),
+        nullable=False
+    )
+    updated_at = Column(
+        TIMESTAMP,
+        server_default=text("CURRENT_TIMESTAMP"),
+        onupdate=text("CURRENT_TIMESTAMP"),
+        nullable=False
+    )
+
+    __table_args__ = (
+        CheckConstraint(
+            "role IN ('admin','site_manager')",
+            name="check_user_role"
+        ),
+    )
