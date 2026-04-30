050cd77cb7d7fdbc7d88e2286ef06665303117e2
Himajacob <himajacob221@gmail.com>
2026-04-30 09:35:54 +0530

    changed schema for invitation based user creation

--- database/models/user.py
+++ database/models/user.py
@@ -11,6 +11,11 @@ class User(Base):
     email = Column(String(255), unique=True, nullable=False)
     password_hash = Column(String, nullable=True)
     role = Column(String(50), nullable=False)
+
+    invite_token = Column(String(255), unique=True, nullable=True, index=True)
+    invite_token_expiry = Column(TIMESTAMP, nullable=True)
+    is_invited = Column(Boolean, nullable=False, server_default=text("false"))
+
     is_active = Column(Boolean, nullable=False, server_default=text("true"))
     created_by = Column(Integer, ForeignKey("users.id"), nullable=True)
     updated_by = Column(Integer, ForeignKey("users.id"), nullable=True)
